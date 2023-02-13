b38cc8a46a3504ecde86e844c5ffe2d7a6500b07
bensammwaniki <bensammwaniki@gmail.com>
2023-02-13 20:20:50 +0300

    Completed the destop view of the home and mobile view for nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './Shared/footer/footer.component';
 import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
+import { HomeComponent } from './Home/home/home.component';
+import { SliderComponent } from './Home/slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component: HomeComponent
+      },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
