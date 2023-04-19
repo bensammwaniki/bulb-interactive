d4fd378b5f117c26cdc43a005b453bf669a6f316
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-04-19 21:13:58 +0300

    Reduced deployment bundle by lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: 'videos', loadChildren: () => import('./video-slider/video-slider.module').then(m => m.VideoSliderModule), outlet: 'video-slider' },
-  { path: 'events', loadChildren: () => import('./video-slider-lazy/video-slider-lazy.module').then(m => m.VideoSliderLazyModule), outlet: 'video-slider-lazy' }
-];
+const routes: Routes = [];
 
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,14 +18,14 @@ import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
 import { HomeComponent } from './Home/home/home.component';
 import { SliderComponent } from './Home/slider/slider.component';
 import { LinksComponent } from './Home/links/links.component';
-// import { VideoSliderComponent } from './Shared/video-slider/video-slider.component';
+import { VideoSliderComponent } from './Shared/video-slider/video-slider.component';
 import { PartnersComponent } from './Home/partners/partners.component';
 import { MagazineComponent } from './magazine/magazine/magazine.component';
 import { MagazineSliderComponent } from './magazine/magazine-slider/magazine-slider.component';
 import { EditionsComponent } from './magazine/editions/editions.component';
 import { VideogalleryComponent } from './Gallery/videogallery/videogallery.component';
 import { CategoryComponent } from './Gallery/category/category.component';
-// import { GallerysliderComponent } from './Gallery/galleryslider/galleryslider.component';
+import { GallerysliderComponent } from './Gallery/galleryslider/galleryslider.component';
 import { EventsComponent } from './Event/events/events.component';
 import { StoryComponent } from './Event/story/story.component';
 import { SpeakersComponent } from './Event/speakers/speakers.component';
@@ -45,14 +45,14 @@ import { AboutTvComponent } from './Home/about-tv/about-tv.component';
     HomeComponent,
     SliderComponent,
     LinksComponent,
-    // VideoSliderComponent,
+    VideoSliderComponent,
     PartnersComponent,
     MagazineComponent,
     MagazineSliderComponent,
     EditionsComponent,
     VideogalleryComponent,
     CategoryComponent,
-    // GallerysliderComponent,
+    GallerysliderComponent,
     EventsComponent,
     StoryComponent,
     SpeakersComponent,
