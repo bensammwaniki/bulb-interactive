c1b6460a7551d24accb34136eab877830c72aacf
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-07-19 13:43:02 +0300

    :sparkles: Introdusing the news section to use wordpress API
    Introducing the news section to use wordpress API

---
+++ src/app/News/display/display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-display',
+  templateUrl: './display.component.html',
+  styleUrls: ['./display.component.scss']
+})
+export class DisplayComponent {
+
+}
