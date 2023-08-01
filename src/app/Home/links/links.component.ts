fdfad327858e63b98d4d85a7d5c95c89380c0b44
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-08-01 16:00:52 +0300

    some commit

--- src/app/Home/links/links.component.ts
+++ src/app/Home/links/links.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NewsService } from 'src/app/news.service';
+
 
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.scss']
 })
-export class LinksComponent {
+export class LinksComponent implements OnInit {
+  mainEvent: any;
+  constructor(private newsService: NewsService) {}
 
+  ngOnInit() {
+    this.getEvents();
+  }
+
+  getEvents() {
+    this.newsService.getEvents().subscribe(
+      (data) => {
+        this.filterMainEvent(data);
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+      }
+    );
+  }
+
+  filterMainEvent(events: any[]) {
+    const today = new Date().getTime();
+    for (const event of events) {
+      const eventDate = new Date(event.acf.date_held).getTime();
+      if (eventDate >= today) {
+        this.mainEvent = event;
+        return;
+      }
+    }
+    this.mainEvent = events[events.length - 1];
+  }
 }
+
