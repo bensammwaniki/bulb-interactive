06dfdab0242e7791a3f755c080eb88c18f9ccca9
Bensam Mwaniki <65493075+bensammwaniki@users.noreply.github.com>
2023-08-02 17:04:04 +0300

    :sparkles: Added new listing page
    added new listing page

---
+++ src/app/Listing/listing/listing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing',
+  templateUrl: './listing.component.html',
+  styleUrls: ['./listing.component.scss']
+})
+export class ListingComponent {
+
+}
